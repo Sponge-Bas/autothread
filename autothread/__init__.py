a6e912eceb90c3db3d98fcb1a34064d5a9339474
Bas de Bruijne <bas.debruijne@canonical.com>
2022-12-22 18:52:17 -0400

    fix windows processing in class bug

--- autothread/__init__.py
+++ autothread/__init__.py
@@ -350,6 +350,9 @@ def multiprocessed(
     """
 
     def _decorator(function):
+        # For windows compatibility we can't pickly the function if its in another
+        # module, so we change the __module__ and add it to the globals.
+        function.__qualname__ = function.__name__
         function.__module__ = __name__
         globals()[function.__name__] = function
         decorator = _Multiprocessed(
